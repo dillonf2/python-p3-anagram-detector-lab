bf0242532af3dfb7d2e31cd1abea7de00c310cae
Frank Dillon <dillonf2@outlook.com>
2023-06-30 17:49:35 -0400

    python anagram detection lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        matched_words = []
+        sorted_word = sorted(self.word.lower())
+        
+        for word in word_list:
+            if sorted(word.lower()) == sorted_word:
+                matched_words.append(word)
+        
+        return matched_words
